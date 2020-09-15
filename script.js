af47e78b2d24a0beaf2ea12c3d4ce509a77a118b
Andrew Crow <andrew@crow.me>
2020-09-14 18:58:23 -0700

    got the question to populate from the object array

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Defining variables for intial setup 
 const leftAside = document.createElement("aside");
 const main = document.createElement("main");
 const rightAside = document.createElement("aside");
 let timerCount = 0;
 
+// Creating the initial page and defining attributes
 function init() {
   document.body.setAttribute("class","container");
   leftAside.setAttribute("id", "highscore");
@@ -17,25 +19,35 @@ function init() {
   document.getElementById('timer').textContent =  timerCount;
 }
 
+// Calling the initial page
 init()
 
+// Building the quiz
 function buildQuiz () {
-  main.innerHTML = "<div><h1>Question</h1></div><div>Answers</div><div>Feedback</div>";
+  main.innerHTML = "<div><h1></h1></div><div></div><div></div>";
   main.setAttribute("class","quiz");
-  main.children[0].setAttribute("id","questionArea");
+  main.children[0].children[0].setAttribute("id","questionArea");
   main.children[1].setAttribute("id","answerArea");
   main.children[2].setAttribute("id","feedbackArea");
   const question = document.getElementById("questionArea");
   const answers = document.getElementById("answerArea");
   const feedback = document.getElementById("feedbackArea");
 
-} 
+  question.textContent = quizQuestions[0].question;
+  
 
+}
+// Logging score for answer and giving feedback
+function logAnswer () {
+
+}
+
+// Building the high scores page
 function highScore () {
   main.innerHTML = "<div><h1>HighScore</h1></div><div>Scores</div><div>Something</div>";
 } 
 
-
+// Quiz timer function
 function startTimer () {
     timerCount = 120;
     var timeInterval = setInterval(function(){
