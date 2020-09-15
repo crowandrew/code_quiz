e07fbdf7fb557105b1269acdc4f33d923a5874b1
Andrew Crow <andrew@crow.me>
2020-09-15 00:36:53 -0700

    added some more questions

--- questions.js
+++ questions.js
@@ -70,42 +70,42 @@ const quizQuestions = [
         correctAnswer: "d"
     },
     {
-        question: "Ask a question8?",
+        question: "Which of the following function of Array object returns the last (greatest) index of an element within the array equal to the specified value, or -1 if none is found?",
         answers: {
-            a: "Something",
-            b: "Also Something",
-            c: "Maybe Something",
-            d: "Could be something"
+            a: "indexOf()",
+            b: "join()",
+            c: "lastIndexOf()",
+            d: "map()"
         },
         correctAnswer: "c"
     },
     {
-        question: "Ask a question8?",
+        question: "10 - Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
         answers: {
-            a: "Something",
-            b: "Also Something",
-            c: "Maybe Something",
-            d: "Could be something"
+            a: "pop()",
+            b: "push()",
+            c: "reduce()",
+            d: "educeRight()"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "What is the HTML tag under which one can write the JavaScript code?",
+        answers: {
+            a: "<javascript>",
+            b: "<scripted>",
+            c: "<script>",
+            d: "<js>"
         },
         correctAnswer: "c"
     },
     {
-        question: "Ask a question8?",
+        question: "What is the correct syntax for referring to an external script called “main.js”?",
         answers: {
-            a: "Something",
-            b: "Also Something",
-            c: "Maybe Something",
-            d: "Could be something"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Ask a question8?",
-        answers: {
-            a: "Something",
-            b: "Also Something",
-            c: "Maybe Something",
-            d: "Could be something"
+            a: "<script src=”main.js”>",
+            b: "<script href=”main.js”>",
+            c: "<script ref=”main.js”>",
+            d: "<script name=”main.js”>"
         },
         correctAnswer: "c"
     },
