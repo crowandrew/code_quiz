7ec9f8e42619620d6d3a110c51229d29577d0892
Andrew Crow <andrew@crow.me>
2020-09-15 01:09:00 -0700

    removed duplicate addListner for start button and made scores plural

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const leftAside = document.createElement("aside");
 const main = document.createElement("main");
 const rightAside = document.createElement("aside");
 const numberQuestions = quizQuestions.length;
-let allHighScores = ["AC - 8","AC - 6","TJ - 9"];
+let allHighScores = [];
 let timerCount = 0;
 let currentQuestion = 0;
 let score = 0;
@@ -12,7 +12,7 @@ let score = 0;
 function init() {
   document.body.setAttribute("class","container");
   leftAside.setAttribute("id", "highscore");
-  leftAside.innerHTML = "View High Score"
+  leftAside.innerHTML = "View High Scores"
   rightAside.innerHTML = "Timer: <span></span>";
   rightAside.children[0].setAttribute("id","timer");
   main.innerHTML = "<div><h1>Coding Quiz Challenge</h1></div><div><p>Try to answer the following code-related questions with the time limit. Keep in mind that incorrect answers will penalize your score 1 point and reduce the timer by ten seconds.</p></div><div><button>Start Quiz</button></div>";
@@ -147,6 +147,7 @@ function highScore () {
   
 } 
 
+// Building the final screen before entering your score
 function gameOver () {
   timerCount = 0;
   document.getElementById('timer').textContent =  timerCount;
@@ -202,14 +203,8 @@ function startTimer () {
     },1000);
 }
 
-// Event Listeners
-  // Event listener for quiz start button
-document.getElementById("startBtn").addEventListener("click", function() {
-  main.innerHTML = "";
-  startTimer();
-  buildQuiz(currentQuestion);
-});
-  // Event listener for HighScore button
+
+// Event listener for HighScore button
 leftAside.addEventListener("click", function(){
   main.innerHTML = "";
   highScore();
